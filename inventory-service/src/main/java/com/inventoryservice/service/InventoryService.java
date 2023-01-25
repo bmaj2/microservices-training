f3ebf06b23f6b5919f491d929be383c479be684c
Bartlomiej Majcher <bartlomiej@ververica.com>
2023-01-25 13:20:05 +0100

    Added Circuit Breaker

--- inventory-service/src/main/java/com/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.inventoryservice.service;
 import com.inventoryservice.dto.InventoryResponse;
 import com.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +12,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
-
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
